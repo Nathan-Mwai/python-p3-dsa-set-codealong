ad90969bf7bc0505969bc19b509c2a84c3b0fe11
Nathan Mwai <nathan.mwai@student.moringaschool.com>
2024-09-11 12:15:58 +0300

    finishes lab

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,29 @@
 class MySet:
-
-    pass
+    def __init__(self,enumerable = []):
+        self.dictionary = {}
+        for value in enumerable:
+            self.dictionary[value] = True
+    
+    def has(self, value):
+        return value in self.dictionary
+    
+    def add(self, value):
+        self.dictionary[value] = True
+        return self
+    
+    def delete(self, value):
+        self.dictionary.pop(value, None)
+        return self
+    
+    def size(self):
+        return len(self.dictionary)
+    
+    def clear(self):
+        self.dictionary.clear()
+    
+    # I did not do this one
+    def __str__(self):
+        list_of_the_set = []
+        for key, value in self.dictionary.items():
+            list_of_the_set.append(str(key))
+        return f'MySet: {{{",".join(list_of_the_set)}}}'
